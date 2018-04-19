134c1b61adbbc2565fb0ee39920bd498480f9bb0
Filipe Gonçalves <filipe@impossible.com>
2018-04-19 15:33:59 +0100

    Finished message board

--- message-board/README.md
+++ message-board/README.md
@@ -1,15 +1,14 @@
 # Example of a message board
 
-Implements a one page message board with cryptographically verified message authors. All crypto/web3 interaction is done client-side. Server is a dumb object store/template renderer.
+Implements a one page message board with cryptographically verified message authors. Almost alll crypto/web3 interaction is done client-side. Server is a dumb object store/template renderer and only validates signatures before inserting them to filter out obvious trash.
 
 
-```
-
+```sh
 python3 -m virtualenv -p python3 venv
 
 source venv/bin/activate
 
-pip install Flask
+pip install Flask web3
 
 FLASK_DEBUG=1 FLASK_APP=main.py python -m flask run
 ```
--- message-board/main.py
+++ message-board/main.py
@@ -1,5 +1,9 @@
 from collections import namedtuple
 from flask import Flask, session, redirect, url_for, escape, request, abort, render_template, jsonify
+from web3 import Web3
+from web3.auto import w3
+
+w3.eth.enable_unaudited_features()
 
 app = Flask(__name__)
 
@@ -13,15 +17,19 @@ def index():
     return render_template('index.html', messages=messages)
 
 
+def verify_sig(msg, sig, addr):
+    recovered_addr = w3.eth.account.recoverMessage(text=msg, signature=sig)
+    return Web3.toChecksumAddress(addr) == Web3.toChecksumAddress(recovered_addr)
+
+
 @app.route('/post', methods=['POST'])
 def post():
     if request.method == 'POST':
         form_data = request.get_json(force=True)
-        if 'message' in form_data and 'signature' in form_data and 'author' in form_data:
-            # TODO
-            # to avoid adding garbage to `messages`,
-            # add web3 dependency and validate the address
-            # prior to appending
+        if 'message' in form_data \
+            and 'signature' in form_data \
+                and 'author' in form_data \
+                and verify_sig(form_data['message'], form_data['signature'], form_data['author']):
             messages.append(
                 Message(form_data['message'], form_data['signature'], form_data['author']))
             return jsonify({'ok': True})
